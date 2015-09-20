363191c06c07097a1d9bbd58431c48e7c333d56b
Alexey Gaziev <alex.gaziev@gmail.com>
2015-09-20 14:06:55 +0800

    Ex 1-15 from lippman

---
+++ lippman/1-15.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+/*
+int main ( {
+    std::cout << "Here is error with missed bracket" << std::endl;
+    }
+*/
+
+/*
+int main() {
+  std::cout << "Here is error with column" << std::endl:
+}
+*/
+
+/*
+int main() {
+  std::cout << Here is error when quotes missing << std::endl;
+}
+*/
+
+/*
+int main() {
+  std::cout << "Here is error when missed second <<" std::endl;
+}
+*/
+
+/*
+int main() {
+  // Error when missed semi-column after retrun
+  return 0
+}
+*/
+
+/*
+int main() {
+  int v1 = 0, v2 = 0;
+  // Error with wrong variable name
+  std::cin >> v >> v2;
+  std::cout << v1 + v2 << std::endl;
+  return 0;
+}
+*/
